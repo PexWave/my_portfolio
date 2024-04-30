1adbc7498fdf961fa0ae0e51ac40ba2206b56a9e
Sarhan Asakil <139829241+PexWave@users.noreply.github.com>
2024-04-30 11:32:58 +0800

    Update axios.jsx

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -4,7 +4,6 @@ const BASE_URL = 'https://3029-58-69-90-10.ngrok-free.app/';
 
 export const baseAxios = axios.create({
     baseURL: BASE_URL,
-    mode: "cors",  // Change the mode to CORS  
 
 });
 
