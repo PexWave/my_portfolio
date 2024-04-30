e362a49dc6c35829a41e846a155dd981d565275e
Sarhan Asakil <139829241+PexWave@users.noreply.github.com>
2024-04-30 12:41:42 +0800

    Update savepersonalinfo.jsx

--- src/api/PUT/savepersonalinfo.jsx
+++ src/api/PUT/savepersonalinfo.jsx
@@ -15,7 +15,9 @@ async function savePersonalInfo(formData, auth) {
         {headers: 
             { 
             'Content-Type': 'multipart/form-data',
-            'Authorization': `Bearer ${auth.access_token}`
+            'Authorization': `Bearer ${auth.access_token}`,
+                        "ngrok-skip-browser-warning": "69420",
+
             },
         })
         .then(response => {
