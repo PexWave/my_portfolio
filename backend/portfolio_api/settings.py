a59adef473c50d6da77bb722d1c8b704fa44b91b
pexwave <asakilsarhan@gmail.com>
2024-06-04 08:19:33 +0800

    supdate

--- backend/portfolio_api/settings.py
+++ backend/portfolio_api/settings.py
@@ -252,11 +252,11 @@ STATIC_FILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 
 STATIC_ROOT = '/static/'
 
-STATIC_URL = '/static/'
 STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
