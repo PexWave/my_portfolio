a376c0164bab9e51830c5e6e0108e9b9b50db623
pexwave <asakilsarhan@gmail.com>
2024-06-04 08:16:08 +0800

    remove secret key

--- backend/portfolio_api/settings.py
+++ backend/portfolio_api/settings.py
@@ -266,5 +266,3 @@ EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = os.environ.get('EMAIL_PORT')
 EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS')
 EMAIL_USE_SSL = os.environ.get('EMAIL_USE_SSL')
-
-
