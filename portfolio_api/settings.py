0d4fce3a9cee1cfc5508f8134dd5a53ff773c6c0
pexwave <asakilsarhan@gmail.com>
2024-06-25 15:39:08 +0800

    update

--- portfolio_api/settings.py
+++ portfolio_api/settings.py
@@ -259,7 +259,7 @@ MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 
 STATIC_ROOT = 'static/'
 
-STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
 
 STATIC_FILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'   
 
